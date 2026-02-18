ee41e1c96681b4e858334fc4a6f5c9eeb3da5685
macooyb <macoybacalla9@gmail.com>
2026-02-18 20:47:16 +0800

    Fresh sync

---
+++ AssignmentApp1/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // CHANGE THIS LINE:
+        setContentView(R.layout.activity_login)  // ← from activity_main to activity_login
+
+        // Also change "R.id.main" to your login layout's root ID
+        // Find the root view ID in activity_login.xml
+        // It might be something like "R.id.login_layout" or "R.id.container"
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_root)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//    }
+//}
